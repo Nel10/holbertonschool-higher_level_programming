7a16c42b68740a910bedeea5cecf665dc8dc1f0d
Nel10 <3461@holbertonschool.com>
2021-10-05 01:09:17 -0500

    Update file 0 and test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+"""
+function return addition
+values: a, b
+Return: a + b
+"""
+
+
 def add_integer(a, b=98):
+    """
+    function return addition
+    """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if type(b) not in  [int, float]:
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
     return a + b
