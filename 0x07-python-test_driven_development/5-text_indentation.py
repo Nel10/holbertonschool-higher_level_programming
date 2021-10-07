0d8d137ee7c583675aab28f45a0186385d9beac2
Nel10 <3461@holbertonschool.com>
2021-10-07 02:45:38 -0500

    file fixe

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -12,12 +12,27 @@ def text_indentation(text):
     """
     if type(text) is not str:
         raise TypeError("text must be a string")
-    my_list = list(text)
-    for i in range(len(my_list)):
-        if my_list[i] == '.' or my_list[i] == '?' or my_list[i] == ':':
-            if my_list[i + 1] and my_list[i + 2]:
-                my_list[i + 1] == '\n'
-                my_list.insert(i + 2, '\n')
-        i += 2
-    my_list = "".join(my_list)
-    print(my_list, end="")
+    flag = 0
+    text = text.strip()
+    for i in text:
+        if flag == 0:
+            if i == ' ':
+                continue
+            else:
+                flag = 1
+        if flag == 1:
+            if i == ':' or i == '?' or i == '.':
+                print(i)
+                print()
+                flag = 0
+            else:
+                print(i, end="")
+#    my_list = list(text)
+#    for i in range(len(my_list)):
+#        if my_list[i] == '.' or my_list[i] == '?' or my_list[i] == ':':
+#            if my_list[i + 1] and my_list[i + 2]:
+#                my_list[i + 1] == '\n'
+#                my_list.insert(i + 2, '\n')
+#        i += 2
+#    my_list = "".join(my_list)
+#    print(my_list, e(nd="")
