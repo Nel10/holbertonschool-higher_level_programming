051c2c09aaa1c804b7a9efb514fcd36ca96d3c48
Nel10 <3461@holbertonschool.com>
2021-10-11 22:45:47 -0500

    file 32

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+function is_kind_of_class
+parametres: obj, a_class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    function return isistance
+    """
+    return isinstance(obj, a_class)
