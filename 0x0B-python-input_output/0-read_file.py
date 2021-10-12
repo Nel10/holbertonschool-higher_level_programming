7510e1093cf632c5ffdd4555eb2cfa4a449f0a2b
Nel10 <3461@holbertonschool.com>
2021-10-12 18:45:03 -0500

    file 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/urs/bin/python3
+"""
+function: read_file
+parametres: filename=""
+"""
+
+
+def read_file(filename=""):
+    """
+    function print a file
+    """
+    with open("my_file_0.txt", encoding="UTF8") as archivo:
+        for line in archivo:
+            print(line)
