a81618f175154d4a31662f42f356f6d874c32cff
Nel10 <3461@holbertonschool.com>
2021-10-11 22:59:23 -0500

    file 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    function raise Exception
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
