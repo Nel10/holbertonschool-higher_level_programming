15cdf84296ded97f23c69a1e9f45051b35faf2c1
Nel10 <3461@holbertonschool.com>
2021-10-12 23:44:23 -0500

    file 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -13,7 +13,7 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """function of class"""
         if attrs is None:
             return self.__dict__
