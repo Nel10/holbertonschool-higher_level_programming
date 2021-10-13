3efaa0acfef65ed5b4b568053aa847378a4182d2
Nel10 <3461@holbertonschool.com>
2021-10-12 23:39:51 -0500

    file 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+class student
+"""
+
+
+class Student:
+    """class student"""
+
+    def __init__(self, first_name, last_name, age):
+        """method init"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """function of class"""
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+        for i in attrs:
+            try:
+                new_dict[i] = self.__dict__[i]
+            except:
+                pass
+        return new_dict
