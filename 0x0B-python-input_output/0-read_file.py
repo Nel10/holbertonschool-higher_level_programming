dbe83568ffc7cf6300846a64e3152b2f2ef150ce
Nel10 <3461@holbertonschool.com>
2021-10-12 19:17:52 -0500

    file 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,5 +9,5 @@ def read_file(filename=""):
     """
     function print a file
     """
-    with open(filename, "r", encoding="utf-8") as f:
+    with open(filename, "r", encoding='utf-8') as f:
         print(f.read(), end="")
