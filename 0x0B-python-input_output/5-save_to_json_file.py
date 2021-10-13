f1b6b3e080a26889d9aef4be37d7943153f7ee58
Nel10 <3461@holbertonschool.com>
2021-10-12 22:56:15 -0500

    file 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+function save_to_json_file
+parametres: my_obj, filename
+"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    with open(filename, 'w', encoding="utf-8") as f:
+        json.dump(my_obj, f)
