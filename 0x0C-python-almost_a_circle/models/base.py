44979af9a1d0db63fc6ef07260dc10da01745801
Nel10 <3461@holbertonschool.com>
2021-10-18 19:25:36 -0500

    class base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """
+create new class Base
 """
 
 
 class Base:
-    """"""
+    """class Base"""
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """"""
+        """inicialize"""
         if id is None:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
