dfa86b1d2f6f934ad33bf47b3674fe6389fd2263
Nel10 <3461@holbertonschool.com>
2021-10-18 21:52:21 -0500

    file

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
 """
+class base
 """
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """"""
+    """class square"""
     def __init__(self, size, x=0, y=0, id=None):
-        """"""
+        """inicialice attr"""
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """"""
+        """property size"""
         return self.width
 
     @size.setter
     def size(self, value):
-        """"""
+        """size setter"""
         self.width = value
         self.height = value
 
     def __str__(self):
-        """"""
+        """function str"""
         string = "[Square] ({:d}) {:d}/{:d} - {:d}"
         string = string.format(self.id, self.x, self.y, self.width)
         return string
