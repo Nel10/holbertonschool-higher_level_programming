76577404b551f6d263c2139488362a320b50db38
Nel10 <3461@holbertonschool.com>
2021-10-18 22:50:04 -0500

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 create new class Base
 """
+import json
 
 
 class Base:
@@ -15,3 +16,10 @@ class Base:
             self.id = self.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """retturn the string"""
+        if list_dictionaries is None:
+            list_dictionaries = []
+        return json.dumps(list_dictionaries)
