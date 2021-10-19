c5c75b29d4e769e63a074aeb5d52fb9572e81d55
Nel10 <3461@holbertonschool.com>
2021-10-18 21:42:52 -0500

    fixed

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -72,5 +72,5 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
-        """area"""
-        return self.width * self.height
+        """ return area """
+        return self.__width * self.__height
