558833f773c7ac1fe0098cef43ad35d989c13147
Nel10 <3461@holbertonschool.com>
2021-11-16 19:29:26 -0500

    full

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- script that prints the full description of the table first_table
 -- from the daatabase hbtn_0c_0
-SHOW FULL TABLE firs_table FROM hbtn_0c_0;
+SHOW CREATE TABLE firs_table FROM hbtn_0c_0;
