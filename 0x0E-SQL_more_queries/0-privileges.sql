1fae3aea8b764db75e84a57c6e1107997965c316
Nel10 <3461@holbertonschool.com>
2021-11-17 20:09:29 -0500

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- script that lists all privileges
-SHOW GRANT FOR user_0d_1'@'localhost;
-SHOW GRANT FOR user_0d_1'@'localhost;
+SHOW GRANTS FOR user_0d_1'@'localhost;
+SHOW GRANTS FOR user_0d_1'@'localhost;
