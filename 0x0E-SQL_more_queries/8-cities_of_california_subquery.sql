7710176a43438ff7008d669c9dfec9cfeea64fd8
Nel10 <3461@holbertonschool.com>
2021-11-17 23:22:14 -0500

    fixed

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,6 @@
 -- script that lists all cities contained in the database hbtn_0d_usa
-SELECT id
-FROM cities
+SELECT id FROM cities
 WHERE state_id = (
-SELECT id
-FROM states
-WHERE name = "California") ORDER BY ASC;
+    SELECT id FROM states
+    WHERE name = "California")
+ORDER BY ASC;
