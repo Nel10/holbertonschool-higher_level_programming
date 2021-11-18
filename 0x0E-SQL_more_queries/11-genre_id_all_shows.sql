94641ee3fe586c085005a65158d4e419dab7853b
Nel10 <3461@holbertonschool.com>
2021-11-18 00:33:07 -0500

    fixed all

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,2 +1,2 @@
--- Import the database dump from hbtn_0d_tvshows
-SELECT tvs.title, tvsg.genre_id FROM tv_shows AS tvs JOIN tv_show_genres AS tvsg WHERE tvs.id=tvsg.show_id ORDER BY tvs.title, tvsg.genre_id ASC;
+-- script that lists all shows contained in the database hbtn_0d_tvshows
+SELECT tvs.title, tvsg.genre_id FROM tv_shows AS tvs LEFT JOIN tv_show_genres AS tvsg ON tvs.id=tvsg.show_id ORDER BY tvs.title, tvsg.genre_id ASC;
--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,2 +1,2 @@
--- Import the database dump from hbtn_0d_tvshows
-SELECT tvs.title, tvsg.genre_id FROM tv_shows AS tvs JOIN tv_show_genres AS tvsg WHERE tvs.id=tvsg.show_id ORDER BY tvs.title, tvsg.genre_id ASC;
+-- shows contained in hbtn_0d_tvshows without a genre linked.
+SELECT ts.title, tg.genre_id FROM tv_shows AS ts LEFT JOIN tv_show_genres AS tg ON ts.id=tg.show_id WHERE tg.genre_id IS NULL ORDER BY ts.title, tg.genre_id ASC;
