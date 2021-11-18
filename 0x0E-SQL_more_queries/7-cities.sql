108c626be2ab14acb8036d88c3d3e69aca2a12c9
Nel10 <3461@holbertonschool.com>
2021-11-17 22:34:49 -0500

    file 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,6 @@
 -- Script that creates the database hbtn_0d_usa and the table cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 stated_id INT NOT NULL,
-name VARCHAR(256) NOT NULL, FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id));
+name VARCHAR(256) NOT NULL,
+FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id));
