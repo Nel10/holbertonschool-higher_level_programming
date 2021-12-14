3625743ab843746e7da72a97b3c2810fed0a00a5
Nel10 <3461@holbertonschool.com>
2021-12-13 20:49:00 -0500

    file 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,9 +3,10 @@ const { argv } = require('process');
 const n = parseInt(argv[2]);
 function factorial (n) {
   if (!n) {
-      return (1);
+    return (1);
   }
   if (n !== 0) {
     return (n * factorial(n - 1));
+  }
 }
 console.log(factorial(n));
