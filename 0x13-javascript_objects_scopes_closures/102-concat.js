cf4fc6be153182c6799ab6cd3625b27d3df82a57
Nel10 <3461@holbertonschool.com>
2021-12-15 21:04:01 -0500

    concat

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const fs = require('fs');
+const file1 = fs.readFileSync('./' + args[0]);
+const file2 = fs.readFileSync('./' + args[1]);
+fs.writeFileSync('./' + args[2], file1 + file2);
