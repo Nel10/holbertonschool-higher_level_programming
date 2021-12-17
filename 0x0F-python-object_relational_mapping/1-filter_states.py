aef2216a8a751b141a3355033e56ab912b4d47e0
Nel10 <3461@holbertonschool.com>
2021-12-17 16:17:35 -0500

    update argv

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,7 +2,7 @@
 import MySQLdb
 from sys import argv
 conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                       passwd=argv[2], db="hbtn_0e_0_usa", charset="utf8")
+                       passwd=argv[2], db=argv[3], charset="utf8")
 cur = conn.cursor()
 cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER by id ASC")
 query_rows = cur.fetchall()
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,8 +4,7 @@ from sys import argv
 conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                        passwd=argv[2], db=argv[3], charset="utf8")
 cur = conn.cursor()
-cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER by id ASC".
-            format(argv[4]))
+cur.execute("SELECT * FROM cities ORDER by id ASC")
 query_rows = cur.fetchall()
 for row in query_rows:
     print(row)
