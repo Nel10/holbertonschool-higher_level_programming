70b8b9ca2f054490c379e06924adcca47e5e8bc0
Nel10 <3461@holbertonschool.com>
2021-12-20 10:37:31 -0500

    update

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+script that deletes all State objects with a name
+containing the letter a from the database hbtn_0e_6_usa
+"""
+
 from sqlalchemy.ext.declarative import declarative_base
 from sys import argv
 from sqlalchemy import create_engine
