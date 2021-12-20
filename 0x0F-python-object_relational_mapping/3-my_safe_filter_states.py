67aa431b89c81ebe16df09450e25cc8cc4f1fec8
Nel10 <3461@holbertonschool.com>
2021-12-20 09:40:16 -0500

    Update

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+"""
+safe from MySQL injections!
+"""
+
 import MySQLdb
 from sys import argv
-conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                       passwd=argv[2], db="hbtn_0e_0_usa", charset="utf8")
-cur = conn.cursor()
-cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER by id ASC",
-            (argv[4],))
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db="hbtn_0e_0_usa", charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER by id ASC",
+                (argv[4],))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
