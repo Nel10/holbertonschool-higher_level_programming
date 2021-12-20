952619a7bc8d2e0eccc1be3054fcb4e6c39153cd
Nel10 <3461@holbertonschool.com>
2021-12-20 09:49:42 -0500

    Update

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """
-safe from MySQL injections!
+script that takes in arguments and displays all values in the states
+table of hbtn_0e_0_usa, safe from MySQL injections!
 """
 
 import MySQLdb
 from sys import argv
+
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db="hbtn_0e_0_usa", charset="utf8")
