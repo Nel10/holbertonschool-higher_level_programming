84fe6c1a7a11e7fac105f7e6eb54e0bd1885b4b4
Nel10 <3461@holbertonschool.com>
2022-01-08 19:17:09 -0500

    Update task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL and displays all HTTP methods the server will accept.
-curl -sI 7be30073c173.0bc3c855.hbtn-cod.io:5000 | grep "Allow" | cut -d' ' -f2-
+curl -sI "$1" | grep "Allow" | cut -d ' ' -f2-
