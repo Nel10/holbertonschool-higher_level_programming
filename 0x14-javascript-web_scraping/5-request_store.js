78e7c51e84652c51a503497972249f0157ac980e
Nel10 <3461@holbertonschool.com>
2022-02-02 00:01:53 -0500

    updated

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,21 +1,15 @@
 #!/usr/bin/node
 const url = process.argv[2];
+const filename = process.argv[3];
 const request = require('request');
+const fs = require('fs');
 
 request(url, function (error, response, body) {
   if (error) {
     console.error('error:', error);
   } else {
-    const jsonBody = JSON.parse(body);
-    const dict = {};
-    for (const task of jsonBody) {
-      if (task.completed === true) {
-        if (dict[task.userId] === undefined) {
-          dict[task.userId] = 0;
-        }
-        dict[task.userId] += 1;
-      }
-    }
-    console.log(dict);
+    fs.writeFile(filename, body, function (err) {
+      if (err) throw err;
+    });
   }
 });
