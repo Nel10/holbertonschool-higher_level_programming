c90202bdd35d40b74735b5b89bb4965e642dedfa
Nel10 <3461@holbertonschool.com>
2022-02-01 23:26:54 -0500

    file updated

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,8 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi.co/api/people/18';
-
-request.get(url, (err, res, body) => {
-  if (err) throw err;
-  console.log(JSON.parse(body).films.length);
+const URL = process.argv[2];
+request(URL, function (err, response, body) {
+  if (err) { console.error(err); }
+  const films = JSON.parse(body);
+  const character = 'https://swapi-api.hbtn.io/api/people/18/';
+  let number = 0;
+  for (let film = 0; film < films.count; film++) {
+    if (films.results[film].characters.indexOf(character) !== -1) {
+      number += 1;
+    }
+  }
+  console.log(number);
 });
